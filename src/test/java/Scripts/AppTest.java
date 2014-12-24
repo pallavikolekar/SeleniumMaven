2bdc5f5b4d8ae579c0c7ba55b8611b9ff1b99023
pallavi <pallavik@glam.com>
2014-12-24 16:20:44 +0530

    changed added

--- src/test/java/Scripts/AppTest.java
+++ src/test/java/Scripts/AppTest.java
@@ -1,16 +1,25 @@
 package Scripts;
 
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class AppTest {
 	WebDriver driver;
 	@Test
-	public void FirstDemo(){
-		driver = new FirefoxDriver();
+	public void FirstDemo() throws MalformedURLException{
+		//driver = new FirefoxDriver();
+		DesiredCapabilities capability = DesiredCapabilities.firefox();
+        capability.setPlatform(Platform.WINDOWS);
+        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capability);
 		driver.get("http://www.google.com");
 		Assert.assertEquals("Google",driver.getTitle());
 		System.out.println("I m in test");
--- src/test/java/Scripts/GoogleScript.java
+++ src/test/java/Scripts/GoogleScript.java
@@ -6,14 +6,14 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class GoogleScript {
-	WebDriver driver;
+	//WebDriver driver;
 
 	@Test
 	public void MyFirstDemo(){
-		driver = new FirefoxDriver();
+		/*driver = new FirefoxDriver();
 		driver.get("http://google.com");
 		Assert.assertEquals("Google", driver.getTitle());
 		System.out.println("MyFirst Demo Script");
-		driver.close();
+		driver.close();*/
 	}
 }
